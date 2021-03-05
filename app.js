caa6909925e42bc9c080fc55620aa711962dda1e
zy <zengyuan@keywa.com>
2021-03-05 09:21:10 +0800

    添加练习

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const Koa = require('koa')
 const KoaRouter = require('koa-router')
 const koaStaticCache = require('koa-static-cache')
 const socketIo = require('socket.io')
+const dayjs = require('dayjs')
 
 const app = new Koa()
 const router = new KoaRouter()
@@ -26,6 +27,7 @@ server.listen(8888)
 const socketServer = new socketIo.Server(server)
 
 socketServer.on('connection', socket => {
+  let time = dayjs(new Date()).format('YYYY-MM-DD HH:mm:ss')
   console.log('有人连接了')
   // console.log(socket)
 
@@ -34,14 +36,16 @@ socketServer.on('connection', socket => {
     // 向当前这个客户端进行消息返送
     socket.emit('serverMessage', {
       id: socket.id,
-      data
+      data,
+      time: time
     })
 
     // 给其它已经链接的客户端（broadcast：不包含自己的）(群聊)
     socket.broadcast.emit('serverMessage', {
       // 从socket中的headers中获取原来登录的时候分配的token
       id: socket.id,
-      data
+      data,
+      time: time
     })
   })
 })
